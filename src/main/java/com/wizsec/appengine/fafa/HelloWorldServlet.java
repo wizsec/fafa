153954bbbab26ce2a5d48650bed4781680aa86fc
J. Maurice <j@wiz.biz>
2015-01-06 16:32:12 +0900

    Fix namespace/directory path issue

--- src/main/java/com/wizsec/appengine/fafa/HelloWorldServlet.java
+++ src/main/java/com/wizsec/appengine/fafa/HelloWorldServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HelloWorldServlet extends HttpServlet {
+  static final long serialVersionUID = 1;
+	
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
       resp.setContentType("text/html");
